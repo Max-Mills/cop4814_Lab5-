4a369b58306a1001a52e8e255bee43f62bc251a9
Max-Mills <33136716+Max-Mills@users.noreply.github.com>
2018-03-20 17:51:44 -0400

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary3
+{
+    public class Game
+    {
+
+        public String Team_1  { get; set; }
+        public String Team_2 { get; set; }
+        public int Team_1_Score { get; set; }
+        public int Team_2_Score { get; set; }
+
+        public Game() { }
+        public Game(String Team_1, String Team_2, int Team_1_Score, int Team_2_Score)
+        {
+            this.Team_1 = Team_1;
+            this.Team_2 = Team_2;
+            this.Team_1_Score = Team_1_Score;
+            this.Team_2_Score = Team_2_Score;
+        }
+
+        public override string ToString()
+        {
+            String gameData = Team_1 + " (" + Team_1_Score + ")  -" + 
+                Team_2 + " (" + Team_2_Score + ")";
+            return String.Format("{0}", gameData);
+        }
+
+
+    }
+}
---
+++ GameFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace ClassLibrary3
+{
+    public class GameFactory
+    {
+        List<Game> gameList = new List<Game>();
+        const String FilePath = @"../../games.xml";
+        Game g = new Game("", "", 0, 0);
+        StreamWriter sw;
+        XmlSerializer Serial;
+
+        public void CreateGameList()
+        {
+            Game g = new Game("Hornets", "Bulldogs", 50, 75);
+            gameList.Add(g);
+            Game  g1 = new Game("Sports", "Ball", 25, 65);
+            gameList.Add(g1);
+            Game g2 = new Game("Bees", "Dogbull", 40, 85);
+            gameList.Add(g2);
+            Game g3 = new Game("Karps", "Sharks", 90, 75);
+            gameList.Add(g3);
+            Game g4 = new Game("Lions", "Wolfs", 80, 45);
+            gameList.Add(g4);
+        }
+
+        public Boolean SerializeGameList()
+        {
+            sw = new StreamWriter(FilePath);
+            Serial = new XmlSerializer(gameList.GetType());
+            Serial.Serialize(sw,gameList);
+            sw.Close();
+            return true;
+        }
+    }
+}
